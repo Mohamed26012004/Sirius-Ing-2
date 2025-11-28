4c4564bd1196c4f6bf9581332a491af2458ec142
Maxime SOUPGUI <maximesoupgui@gmail.com>
2025-11-28 13:59:40 +0100

    Update back.js

--- proto-front/src/constants/back.js
+++ proto-front/src/constants/back.js
@@ -1,5 +1,5 @@
 
-export const LOCAL_HOST = 'http://localhost:8081';
+export const LOCAL_HOST = 'http://172.31.252.87:8081';
 export const LOCAL_HOST_SAMPLE = LOCAL_HOST + '/sample/';
 export const GET_SAMPLES = LOCAL_HOST_SAMPLE + 'all'
 export const UPDATE_SAMPLES = LOCAL_HOST_SAMPLE + 'update'
